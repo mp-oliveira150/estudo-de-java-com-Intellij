084f147ce0bdc63f676cbe472f769bdb01963b6f
mp-oliveira1 <marcospaulomax4@gmail.com>
2023-02-20 16:21:51 -0400

    Java básico. Configuração inicial.

---
+++ src/mp_oliveira/Programa1.java
@@ -0,0 +1,12 @@
+package mp_oliveira;
+
+import mp_oliveira.Models.Gato;
+
+public class Programa1 {
+
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+    }
+}
---
+++ src/mp_oliveira/Teste.java
@@ -0,0 +1,13 @@
+package mp_oliveira;
+
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+
+    /*vfdvdff*/
+
+
+
+}
