59d4ffc9c2851bcbee19e4d6023757161e02867e
mp-oliveira1 <marcospaulomax4@gmail.com>
2023-03-13 22:33:16 -0400

    Usando Debug.

---
+++ src/mp_oliveira/funcoes/Cauculadora.java
@@ -0,0 +1,36 @@
+package mp_oliveira.funcoes;
+
+import java.util.Scanner;
+
+public class Cauculadora {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x, y;
+        System.out.println("Digite o primeiro valor: ");
+        x = scanner.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        y = scanner.nextInt();
+
+        int somar = somar(x, y);
+        int subtrair = subtrair(x, y);
+        int multiplicar = multiplicar(x, y);
+        float dividir = dividir(x, y);
+
+        System.out.println("Soma = " + somar);
+        System.out.println("Subtração = " + subtrair);
+        System.out.println("Multiplicação = " + multiplicar);
+        System.out.println("Divisão = " + dividir);
+    }
+    public static int somar(int x, int y){
+        return x + y;
+    }
+    public static int subtrair(int x, int y){
+        return x - y;
+    }
+    public static int multiplicar(int x, int y){
+        return x * y;
+    }
+    public static float dividir(int x, int y){
+        return x / y;
+    }
+}
