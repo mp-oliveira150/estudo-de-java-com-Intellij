d4ba6a7061842d584b6675993cb94c98c8f16e44
mp-oliveira1 <marcospaulomax4@gmail.com>
2023-07-01 20:39:02 -0400

    Adição do exercício Consoantes no package arrays_em_java. Correção ortográfica do nome do package Estruturas_de_repaticao para Estruturas_de_repeticao.

--- src/edu/mp_oliveira/aprendendo_sintaxe_java_DIO/arrays_em_java/Ex2_Consoantes.java
+++ src/edu/mp_oliveira/aprendendo_sintaxe_java_DIO/arrays_em_java/Ex2_Consoantes.java
@@ -1,7 +1,38 @@
 package edu.mp_oliveira.aprendendo_sintaxe_java_DIO.arrays_em_java;
 
+import java.util.Scanner;
+
 public class Ex2_Consoantes {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] vetor_de_consoantes = new String[6];
+        int quantConsoantes = 0;
+        int count = 0;
+
+        do {
+            System.out.print("Digite uma letra: \n");
+            String letra = scan.next();
+            if (    !(letra.equalsIgnoreCase("a")|
+                     letra.equalsIgnoreCase("e")|
+                     letra.equalsIgnoreCase("i")|
+                     letra.equalsIgnoreCase("o")|
+                     letra.equalsIgnoreCase("u"))) {
+                vetor_de_consoantes [count] = letra;
+                quantConsoantes ++;}
+
+            count ++;
+
+        }while (count < vetor_de_consoantes.length);
+
+        System.out.println("Há " + quantConsoantes + " Consoantes no vetor.");
+
+        System.out.println("Consoantes: ");
+        for (String consoantes: vetor_de_consoantes) {
+            if (consoantes != null){
+            System.out.print(consoantes + ", ");
+            }
+        }
 
     }
 }
